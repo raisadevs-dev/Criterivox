aa09d9e9adbd574c8eb2b85afd156c89a0fb0ace
Rajnandini Bhoi <raisadevs@gmail.com>
2026-09-07 18:38:48 +0530

    feat: add day and night theme control

--- presentation/lib/main.dart
+++ presentation/lib/main.dart
@@ -1,4 +1,48 @@
 import 'package:flutter/material.dart';
 import 'app_shell.dart';
 
-void main()=>runApp(const MaterialApp(title:'Criterivox',debugShowCheckedModeBanner:false,home:CriterivoxShell()));
+class CriterivoxApp extends StatefulWidget {
+  const CriterivoxApp({super.key});
+
+  @override
+  State<CriterivoxApp> createState() => _CriterivoxAppState();
+}
+
+class _CriterivoxAppState extends State<CriterivoxApp> {
+  ThemeMode _themeMode = ThemeMode.dark;
+
+  void _toggleTheme() {
+    setState(() {
+      _themeMode = _themeMode == ThemeMode.dark
+          ? ThemeMode.light
+          : ThemeMode.dark;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Criterivox',
+      debugShowCheckedModeBanner: false,
+      themeMode: _themeMode,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        colorSchemeSeed: const Color(0xFF6F5AEF),
+        scaffoldBackgroundColor: const Color(0xFFF5F6FA),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        colorSchemeSeed: const Color(0xFF8D78FF),
+        scaffoldBackgroundColor: const Color(0xFF050712),
+        useMaterial3: true,
+      ),
+      home: CriterivoxShell(
+        isDarkMode: _themeMode == ThemeMode.dark,
+        onToggleTheme: _toggleTheme,
+      ),
+    );
+  }
+}
+
+void main() => runApp(const CriterivoxApp());
