a16f7fa5d2941d644174af09ced8eab6fb97b699
Rajnandini Bhoi <raisadevs@gmail.com>
2026-09-17 01:15:07 +0530

    feat(s8): export research and S7 contract adapters

--- src/criterivox/s8/__init__.py
+++ src/criterivox/s8/__init__.py
@@ -5,6 +5,8 @@ from .interventions import HumanIntervention, InterventionRegistry, RevisionReco
 from .models import Artifact, ArtifactKind, BureauEvent, VerificationResult
 from .persistence import S8SQLiteStore
 from .policy import AccessRequest, AuthorizationError, S8Policy
+from .research import EvaluationRecorder, ExperimentRecord, MemoryConsolidator, TemporalRetriever
+from .s7_adapter import S7Adapter, S7ArtifactEnvelope
 
 __all__ = [
     "AccessRequest",
@@ -12,11 +14,17 @@ __all__ = [
     "ArtifactKind",
     "AuthorizationError",
     "BureauEvent",
+    "EvaluationRecorder",
     "EvidenceResearchBureau",
+    "ExperimentRecord",
     "HumanIntervention",
     "InterventionRegistry",
+    "MemoryConsolidator",
     "RevisionRecord",
+    "S7Adapter",
+    "S7ArtifactEnvelope",
     "S8Policy",
     "S8SQLiteStore",
+    "TemporalRetriever",
     "VerificationResult",
 ]
