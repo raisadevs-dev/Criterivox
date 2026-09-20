d680622118b2de4b256cc15694af371cca2a1bfc
Rajnandini Bhoi <raisadevs@gmail.com>
2026-09-20 20:57:06 +0530

    Route action-preparation chip through capability registry

--- src/criterivox/character_backbone/capability_discovery.py
+++ src/criterivox/character_backbone/capability_discovery.py
@@ -12,7 +12,7 @@ class CapabilityMatch:
 _INTENT_CAPABILITIES={
  "QUERY_PAST_STATE":"query_current_task_state","QUERY_CURRENT_STATE":"query_current_task_state","QUERY_NEXT_STATE":"query_current_task_state",
  "EXPLAIN":"trace_provenance","SHOW_PROVENANCE":"trace_provenance","VERIFY":"verify_claim","CHALLENGE":"record_human_challenge",
- "RECOMMEND":"produce_recommendation","SHOW_OPTIONS":"build_plan","EXECUTE":"prepare_action","KNOWLEDGE":"synthesize_knowledge",
+ "RECOMMEND":"produce_recommendation","SHOW_OPTIONS":"build_plan","PREPARE_ACTION":"prepare_action","EXECUTE":"prepare_action","KNOWLEDGE":"synthesize_knowledge",
  "HANDOFF":"transfer_artifact","CHANGE_REQUIREMENT":"adapt_context","CAPABILITY_DISCOVERY":"query_current_task_state",
 }
 def discover(intent:str, character_id:str|None=None)->tuple[CapabilityMatch,...]:
