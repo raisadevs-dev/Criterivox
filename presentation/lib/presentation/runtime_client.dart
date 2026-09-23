236ddb93337c92e6468c14cd4e1e420450d53874
Rajnandini Bhoi <raisadevs@gmail.com>
2026-09-23 14:03:14 +0530

    Attach application language mode to global chat messages

--- presentation/lib/presentation/runtime_client.dart
+++ presentation/lib/presentation/runtime_client.dart
@@ -251,6 +251,7 @@ class CharacterRuntimeClient {
         'target_character': targetCharacter,
         if (taskId != null) 'task_id': taskId,
         'message': message,
+        'language_mode': languageMode,
         'data': data,
         'context': context,
         'references': references,
