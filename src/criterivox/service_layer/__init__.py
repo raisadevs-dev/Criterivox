95f6a6faab6c62cc730174d2ae6888d195a1dcf7
Rajnandini Bhoi <raisadevs@gmail.com>
2026-09-26 00:22:14 +0530

    feat(service-layer): add reusable service composition contracts

---
+++ src/criterivox/service_layer/__init__.py
@@ -0,0 +1,3 @@
+from .contracts import OutcomeRecord,ServicePlan,ServiceRequest,ServiceResult
+from .composer import ServiceComposer,service_composer
+__all__=["OutcomeRecord","ServicePlan","ServiceRequest","ServiceResult","ServiceComposer","service_composer"]
